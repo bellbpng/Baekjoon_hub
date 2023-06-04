c1611a2bb63f0599494053bd1e3ef58af46d3f70
Bokyoung <bellbpng@naver.com>
2023-06-04 16:08:33 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -1,120 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 
-int N;
-
-// NxN 행렬곱 연산 함수
-vector<vector<int>> matrix_mul(vector<vector<int>>& front_matrix, vector<vector<int>>& back_matrix)
+vector<vector<int>> matrix_mul(vector<vector<int>>& front_mat, vector<vector<int>>& back_mat)
 {
-	vector<int> row, col;
-	vector<vector<int>> new_matrix(N, vector<int>(N));
-	for (int r = 0; r < N; r++)
-	{
-		// r행에 해당하는 원소를 담는다.
-		for (int c = 0; c < N; c++)
-			row.push_back(front_matrix[r][c]);
-		
-		// 행렬곱을 통해 원소 값을 계산한다.
-		for (int c = 0; c < N; c++)
-		{
-			// row 원소와 곱할 열 원소를 담는다.
-			for (int r = 0; r < N; r++)
-				col.push_back(back_matrix[r][c]);
-			// row 원소와 col 원소를 곱한다.
-			int ele = 0;
-			for (int i = 0; i < N; i++)
-				ele += row[i] * col[i];
-			new_matrix[r][c] = ele % 1000;
-			col.clear();
-		}
-		row.clear();
-	}
-	return new_matrix;
-}
+	int n = front_mat[0].size();
+	vector<vector<int>> rmat(n, vector<int>(n,0));
 
-bool check_matrix(vector<vector<int>>& matrix1, vector<vector<int>>& matrix2)
-{
-	for (int r = 0; r < N; r++)
-		for (int c = 0; c < N; c++)
-			if (matrix1[r][c] != matrix2[r][c])
-				return false;
-	return true;
-}
-
-void matrix_b_square(vector<vector<int>>& front_matrix, vector<vector<int>>& back_matrix, long long b)
-{
-	// 남은 곱셈 횟수(b)가 짝수인 경우
-	if (b % 2 == 0)
-	{
-		if (check_matrix(front_matrix, back_matrix)) // front_matrix와 back_matrix가 같은 경우
-		{
-			front_matrix = matrix_mul(front_matrix, front_matrix);
-			back_matrix = front_matrix;
-		}
-		else
-		{
-			back_matrix = matrix_mul(front_matrix, back_matrix);
-			front_matrix = matrix_mul(front_matrix, front_matrix);
+	for(int r=0; r<n; r++) {
+		for(int c=0; c<n; c++) {
+			int ele=0;
+			for(int i=0; i<n; i++) {
+				ele += front_mat[r][i]*back_mat[i][c];
+			}
+			rmat[r][c] = ele%1000;
 		}
 	}
-	else // 남은 곱셈 횟수(b)가 홀수인 경우
-	{
-		front_matrix = matrix_mul(front_matrix, front_matrix);
-		back_matrix = matrix_mul(front_matrix, back_matrix);
-	}
+	return rmat;
 }
 
-void print_matrix(vector<vector<int>>& matrix)
+void print_matrix(vector<vector<int>>& mat)
 {
-	for (int r = 0; r < N; r++) {
-		for (int c = 0; c < N; c++)
-			cout << matrix[r][c] << " ";
+	int n = mat[0].size();
+	for(int r=0; r<n; r++) {
+		for(int c=0; c<n; c++)
+			cout << mat[r][c] << " ";
 		cout << endl;
 	}
 }
 
+void pow_matrix(vector<vector<int>>& front_mat, vector<vector<int>>& back_mat, long long B)
+{	
+	// B가 1인 경우 그대로 출력
+	if(B==1) {
+		print_matrix(front_mat);
+		return;
+	}
+	int n = front_mat[0].size();
+	vector<vector<int>> ans_mat(n, vector<int>(n,0));
+
+	while(B>=2) {
+		if(B%2==0) { // 짝수인 경우 
+			back_mat = matrix_mul(front_mat, back_mat);
+			front_mat = matrix_mul(front_mat, front_mat);
+		}	
+		else { // 홀수인 경우
+			front_mat = matrix_mul(front_mat, front_mat);
+			back_mat = matrix_mul(front_mat, back_mat);
+		}
+		ans_mat = back_mat;
+		B/=2;
+	}	
+	print_matrix(ans_mat);
+}
+
 int main()
 {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
+	int N;
 	long long B;
 	cin >> N >> B;
-	vector<vector<int>> matrix(N, vector<int>(N));
-	for (int r = 0; r < N; r++)
-		for (int c = 0; c < N; c++)
-		{
+
+	vector<vector<int>> front_matrix(N,vector<int>(N,0));
+	vector<vector<int>> back_matrix(N,vector<int>(N,0));
+
+	for(int r=0; r<N; r++)
+		for(int c=0; c<N; c++) {
 			int val;
 			cin >> val;
-			matrix[r][c] = val%1000;
+			front_matrix[r][c] = val%1000;
 		}
+			
 
-	vector<vector<int>> front_matrix = matrix;
-	vector<vector<int>> back_matrix = matrix;
-	vector<vector<int>> ans_matrix = matrix_mul(front_matrix, back_matrix);
-
-	while (true)
-	{
-		if (B == 1)
-		{
-			ans_matrix = front_matrix;
-			break;
-		}
-		if (B == 2)
-		{
-			ans_matrix = matrix_mul(front_matrix, back_matrix);
-			break;
-		}
-		matrix_b_square(front_matrix, back_matrix, B);
-		if (B == 3)
-		{
-			ans_matrix = back_matrix;
-			break;
-		}
-		B /= 2;
-	}
-	print_matrix(ans_matrix);
+	back_matrix = front_matrix;
+	pow_matrix(front_matrix, back_matrix, B);
 	return 0;
 }
