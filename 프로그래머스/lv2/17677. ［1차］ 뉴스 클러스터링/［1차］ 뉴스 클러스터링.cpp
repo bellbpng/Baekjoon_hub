411224621911bf1831cd6dab57cf7ec0fdf1a466
Bokyoung <bellbpng@naver.com>
2023-07-19 23:36:32 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.cpp
@@ -1,83 +1,43 @@
 #include <string>
+#include <vector>
+#include <cctype>
 #include <algorithm>
 #include <iostream>
-#include <cctype>
 
 using namespace std;
-
-vector<string> init_set(string str){
-    vector<string> str_set;
-    string tmp;
-    for(int i=0; i<str.size(); i++){
-        if(isalpha(str[i])){
-            if(isupper(str[i]))
-                tmp.push_back(tolower(str[i]));
-            else
-                tmp.push_back(str[i]);
-        }
-       if(isalpha(str[i+1])){
-            if(isupper(str[i+1]))
-                tmp.push_back(tolower(str[i+1]));
-            else
-                tmp.push_back(str[i+1]);
-        }
-        if(tmp.size()==2)
-            str_set.push_back(tmp);
-        tmp.clear();
-    }
-    return str_set;
-}
-
-float get_jacard(vector<string>& short_set, vector<string>& long_set){
-    int inter_count = 0;
-    int union_count = 0;
-    float ret = 0.0f;
-    int long_len = long_set.size();
-    int short_len = short_set.size();
-    vector<int> checked(long_len, 0);
-    vector<int> checked_s(short_len, 0);
-    
-    string ref;
-    for(int i=0; i<short_set.size(); i++){
-        ref = short_set[i];
-        for(int j=0; j<long_set.size(); j++){
-            if(ref==long_set[j] && checked[j]==0){
-                inter_count++;
-                checked[j]=1;
-                checked_s[i]=1;
-                break;
-            }
-        }
-    }
-    
-    union_count = inter_count;
-    for(int i=0; i<short_set.size(); i++){
-        if(checked_s[i]==0)
-            union_count++;
-    }
-    for(int i=0; i<long_set.size(); i++){
-        if(checked[i]==0)
-            union_count++;
-    }
-    ret = (float)inter_count / union_count;
-    
-    return ret;
-}
-
 
 int solution(string str1, string str2) {
     int answer = 0;
     // init
-    vector<string> str1_set = init_set(str1);
-    vector<string> str2_set = init_set(str2);   
-    vector<string> short_set = str1_set < str2_set ? str1_set : str2_set;
-    vector<string> long_set = str1_set > str2_set ? str1_set : str2_set;
+    // 가능한 모든 조합
+    vector<int> set_1(26*26, 0);
+    vector<int> set_2(26*26, 0);
     
-    // A=B
-    if(short_set == long_set) return 65536;
+    for(int i=0; i<str1.size(); i++){
+        if(isalpha(str1[i]) && isalpha(str1[i+1])){
+            if(isupper(str1[i])) str1[i]=tolower(str1[i]);
+            if(isupper(str1[i+1])) str1[i+1]=tolower(str1[i+1]);
+            set_1[(str1[i]-'a')*26 + (str1[i+1]-'a')]++;
+        }
+    }
     
-    float jacard = get_jacard(short_set, long_set);
-    answer = int(jacard * 65536);
+    for(int i=0; i<str2.size(); i++){
+        if(isalpha(str2[i]) && isalpha(str2[i+1])){
+            if(isupper(str2[i])) str2[i]=tolower(str2[i]);
+            if(isupper(str2[i+1])) str2[i+1]=tolower(str2[i+1]);
+            set_2[(str2[i]-'a')*26 + (str2[i+1]-'a')]++;
+        }
+    }
+    
+    int inter_cnt = 0, union_cnt=0;
+    for(int i=0; i<set_1.size(); i++){
+        inter_cnt += min(set_1[i], set_2[i]);
+        union_cnt += max(set_1[i], set_2[i]);
+    }
+    if(union_cnt==0) 
+        answer = 65536;
+    else
+        answer = int(((float)inter_cnt/union_cnt) * 65536);
     
     return answer;
 }
