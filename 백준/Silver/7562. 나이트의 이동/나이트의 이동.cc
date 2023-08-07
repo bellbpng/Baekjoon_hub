16d6bce3198247d8c0b9428c86026a9d074cfce6
Bokyoung <bellbpng@naver.com>
2023-08-08 00:02:33 +0900

    [Silver I] Title: 나이트의 이동, Time: 28 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -4,35 +4,36 @@
 #include <queue>
 using namespace std;
 
-int knightMove[8][2] = { {-1, -2}, {-2,-1}, {-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2} };
-
+int knightMove[8][2] = {
+	{-1, -2}, {-2,-1}, {-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}
+};
 
 typedef struct _posInfo {
 	int x;
 	int y;
+	int idx;
 } Pos;
 
-void Bfs(int curX, int curY, int targetX, int targetY, int l) {
-	vector<vector<int>> board(l, vector<int>(l, 100000));
+int Bfs(int curX, int curY, int targetX, int targetY, int l) {
+	vector<vector<bool>> visited(l, vector<bool>(l, false));
 	queue<Pos> q;
-	board[curX][curY] = 0;
-	q.push({ curX, curY });
+	q.push({ curX, curY, 0 });
 
 	while (!q.empty()) {
 		Pos cpos = q.front();
+		visited[cpos.x][cpos.y] = true;
 		q.pop();
+		if (cpos.x == targetX && cpos.y == targetY) return cpos.idx;
+
 		for (int dir = 0; dir < 8; dir++) {
 			int nextX = cpos.x + knightMove[dir][0];
 			int nextY = cpos.y + knightMove[dir][1];
-			if (nextX >= 0 && nextX < l && nextY >= 0 && nextY < l) {
-				if (board[nextX][nextY] > board[cpos.x][cpos.y]+1) {
-					board[nextX][nextY] = board[cpos.x][cpos.y] + 1;
-					q.push({ nextX, nextY });
-				}
-			}
+			if (nextX < 0 || nextX >= l || nextY < 0 || nextY >= l) continue;
+			if (visited[nextX][nextY]) continue;
+			visited[nextX][nextY] = true;
+			q.push({ nextX, nextY, cpos.idx + 1 });
 		}
 	}
-	printf("%d\n", board[targetX][targetY]);
 }
 
 
@@ -44,9 +45,13 @@ int main()
 	scanf("%d", &T);
 
 	for (int testCase = 0; testCase < T; testCase++) {
-		int l, curX, curY, targetX, targetY;
-		scanf("%d %d %d %d %d", &l, &curX, &curY, &targetX, &targetY);
-		Bfs(curX, curY, targetX, targetY, l);
+		int l, ans=0;
+		scanf("%d", &l);
+		int curX, curY, targetX, targetY;
+		scanf("%d %d", &curX, &curY);
+		scanf("%d %d", &targetX, &targetY);
+		ans = Bfs(curX, curY, targetX, targetY, l);
+		printf("%d\n", ans);
 	}
 	return 0;
 }
