fccec819019a4b786b7916db47d19104f8afa782
Bokyoung <bellbpng@naver.com>
2023-09-01 10:54:38 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.cpp
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.cpp
@@ -1,105 +1,54 @@
 #include <string>
 #include <vector>
 #include <cctype>
-#include <memory.h>
-#include <algorithm>
-#include <cmath>
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-vector<char> allCase, cand, op;
-vector<long long> num;
-long long ans = 0, result = 0;
-bool visited[3] = {false, };
+vector<string> candOp = {"+-*", "+*-", "-+*", "-*+", "*+-", "*-+"};
 
-// 숫자가 작을수록 높은 우선순위
-int getPriority(char oper){
-    for(int i=0; i<cand.size(); i++){
-        if(cand[i]==oper) return i;
-    }
-}
-
-void getResult(vector<char> iop, vector<long long> inum){
-    if(iop.size()==0) {
-        result = abs(inum[0]);
-        cout << result << endl;
-        return;
-    }
-    
-    vector<char> nop;
-    vector<long long> nnum;
-    // 가장 높은 우선순위 연산자 찾기
-    int priority = getPriority(iop[0]);
-    for(int i=1; i<iop.size(); i++){
-        int np = getPriority(iop[i]);
-        if(priority > np) priority = np;
-    }
-    
-    bool flag = true;
-    long long val;
-    int idx = 0;
-    for(int i=0; i<iop.size(); i++){
-        if(priority==getPriority(iop[i]) && flag){
-            if(iop[i]=='+') val = inum[idx] + inum[idx+1];
-            else if(iop[i]=='-') val = inum[idx] - inum[idx+1];
-            else if(iop[i]=='*') val = inum[idx] * inum[idx+1];
-            nnum.push_back(val);
-            idx+=2;
-            flag = false;
-        }
-        else{
-            nnum.push_back(inum[idx++]);
-            nop.push_back(iop[i]);
+long long getValue(string prior, vector<char>& top, vector<long long>& tnums){
+    for(int j=0; j<3; j++){
+        int idx=0;
+        char pr = prior[j];
+        for(int t=0; t<top.size(); t++){
+            if(pr==top[t]){
+                if(pr=='+') tnums[t] += tnums[t+1];
+                else if(pr=='-') tnums[t] -= tnums[t+1];
+                else if(pr=='*') tnums[t] *= tnums[t+1];
+                tnums.erase(tnums.begin()+t+1);
+                top.erase(top.begin()+t);
+                t--;
+            }
         }
     }
-    
-    getResult(nop, nnum);
-}
-
-void setPriority(int v){
-    if(cand.size()==allCase.size()) {
-        // 수식 계산
-        vector<char> iop = op;
-        vector<long long> inum = num;
-        getResult(iop, inum);
-        ans = max(ans, result);
-        return;
-    }
-    
-    for(int i=0; i<allCase.size(); i++){
-        if(visited[i]) continue;
-        visited[i]=true;
-        cand.push_back(allCase[i]);
-        setPriority(i+1);
-        cand.pop_back();
-        visited[i]=false;
-    }
+    return abs(tnums[0]);
 }
 
 long long solution(string expression) {
     long long answer = 0;
-    
-    // 0: +, 1: -, 2: *
-    vector<char> opCount(3,0);
+    vector<char> op;
+    vector<long long> nums;
     string tmp;
     for(int i=0; i<expression.length(); i++){
         char ch = expression[i];
         if(isdigit(ch)) tmp.push_back(ch);
         else {
-            num.push_back(stoll(tmp));
+            nums.push_back(stoll(tmp));
             tmp.clear();
             op.push_back(ch);
-            if(ch=='+') opCount[0]++;
-            else if(ch=='-') opCount[1]++;
-            else if(ch=='*') opCount[2]++;
         }
     }
-    num.push_back(stoll(tmp));
-    if(opCount[0]>0) allCase.push_back('+');
-    if(opCount[1]>0) allCase.push_back('-');
-    if(opCount[2]>0) allCase.push_back('*');
+    nums.push_back(stoll(tmp));
     
-    setPriority(0);
-    answer = ans;
+    for(int i=0; i<6; i++){
+        vector<char> top = op;
+        vector<long long> tnums = nums;
+        string prior = candOp[i];
+        answer = max(answer, getValue(prior, top, tnums));
+        // cout << answer << endl;
+    }
+    
     return answer;
 }
